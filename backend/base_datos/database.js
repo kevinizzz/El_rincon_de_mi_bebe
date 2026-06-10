5cba334b13ab44039a39018577cb5c25d52a483a
kevinizzz <zapatamedranokevinanibal@gmail.com>
2026-06-10 10:54:41 -0600

    Mensaje descriptivo aquí

--- backend/base_datos/database.js
+++ backend/base_datos/database.js
@@ -1,13 +1,12 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config({ path: './backend/.env' });
+require('dotenv').config();  // ← sin ruta
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-
+    port: process.env.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
@@ -16,9 +15,7 @@ const pool = mysql.createPool({
 (async () => {
     try {
         const connection = await pool.getConnection();
-
         console.log('✅ Conexión MySQL exitosa');
-
         connection.release();
     } catch (error) {
         console.error('❌ Error MySQL:', error.message);
